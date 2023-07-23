cadda1e71c06531942d5bb4e7c151e4d286ed525
Matheus Rambo <matheusrambo24@gmail.com>
2023-07-23 19:35:58 -0300

    refactor: Use ByteBuffer to parse directly instead of creating a new vector

--- include/easykey.hpp
+++ include/easykey.hpp
@@ -1,24 +1,29 @@
 #pragma once
 
-#include "know_nothing.hpp"
+#include "byte_buffer.hpp"
 
 #include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
 
+namespace knownothing
+{
+/**
+ * The availables Know Nothing Protocol Versions
+ */
+enum Protocol : std::uint8_t
+{
+    V1 = 0x01,
+};
+}  // namespace knownothing
+
 namespace easykey
 {
 struct RequestMessage
 {
   public:
-    /**
-     * Constructs the Message object
-     * NOTE: The @param input must be a valid Know Nothing protocol message,
-     * otherwise, we will have an undefined behavior
-     */
-    static std::unique_ptr<RequestMessage> read(
-        std::vector<std::uint8_t> input);
+    static std::unique_ptr<RequestMessage> read(easykey::ByteBuffer& buffer);
     const knownothing::Protocol kn_protocol;
     const std::string key;
     const std::vector<std::uint8_t> value;
--- source/easykey.cpp
+++ source/easykey.cpp
@@ -1,5 +1,4 @@
 #include "easykey.hpp"
-#include "know_nothing.hpp"
 
 #include <array>
 #include <cstdint>
@@ -7,6 +6,7 @@
 #include <memory>
 #include <regex>
 #include <string>
+#include "byte_buffer.hpp"
 
 using namespace easykey;
 using namespace std;
@@ -15,28 +15,20 @@ using namespace knownothing;
 static const string regex_value = "[^a-zA-Z0-9_]+";
 static const regex invalid_key_regex(regex_value);
 
-static bool is_key_valid(const string &key)
+static bool is_key_valid(const string& key)
 {
     return !key.empty() && !regex_search(key, invalid_key_regex);
 }
 
-unique_ptr<RequestMessage> RequestMessage::read(vector<uint8_t> input)
+unique_ptr<RequestMessage> RequestMessage::read(ByteBuffer& buffer)
 {
-    if (!check_integrity(input))
-    {
-        // Would be nice to use optional ...
-        return unique_ptr<RequestMessage>(nullptr);
-    }
-
-    uint32_t message_index = 0;
-
     /**
      * First byte is the protocol version
      */
     const auto protocol =
-        static_cast<knownothing::Protocol>(input[message_index++]);
+        static_cast<knownothing::Protocol>(buffer.get_integer1());
 
-    const auto messages_number = input[message_index++];
+    const auto messages_number = buffer.get_integer1();
     switch (messages_number)
     {
         case 1:
@@ -56,19 +48,18 @@ unique_ptr<RequestMessage> RequestMessage::read(vector<uint8_t> input)
     /**
      * Next four bytes are the bytes that compose the first message 4 byte size
      */
-    const auto first_message_size = deserialize({input[message_index++],
-                                                 input[message_index++],
-                                                 input[message_index++],
-                                                 input[message_index++]});
+    const auto first_message_size = buffer.get_integer4();
 
     /**
      * From the fifth byte until first messge size, its the first message
-     * https://cplusplus.com/reference/string/string/string/
      */
-    const auto first_message =
-        string(((char *)input.data()) + message_index, first_message_size);
-
-    message_index += first_message_size;
+    const auto teste = buffer.get_next(first_message_size);
+    string first_message;
+    for (const auto& c : teste)
+    {
+        char c1 = (char)c;
+        first_message += c1;
+    }
 
     /**
      * Check if the first message is a valid key value
@@ -87,27 +78,13 @@ unique_ptr<RequestMessage> RequestMessage::read(vector<uint8_t> input)
      * After the first message, there are four bytes that compose the second
      * message size
      */
-    const auto second_message_size = deserialize({input[message_index++],
-                                                  input[message_index++],
-                                                  input[message_index++],
-                                                  input[message_index++]});
+    const auto second_message_size = buffer.get_integer4();
 
     /**
      * Creates the vector for the second message.
      * NOTE: This is just a raw byte array, so no data is checked!
      */
-    vector<uint8_t> second_message;
-
-    // reserve the desired bytes for the message
-    second_message.reserve(second_message_size);
-
-    // pointer arithmetic to point to the beginning of the second message
-    uint8_t *second_message_array = input.data() + message_index;
-
-    for (uint32_t index = 0; index < second_message_size; index++)
-    {
-        second_message.push_back(second_message_array[index]);
-    }
+    vector<uint8_t> second_message = buffer.get_next(second_message_size);
 
     return unique_ptr<RequestMessage>(
         new RequestMessage{protocol, first_message, second_message});
@@ -135,19 +112,16 @@ const vector<uint8_t> ResponseMessage::write() const
     // byte 5
     response.push_back(status_code);
 
-    // serialize the value size
-    const auto value_size = serialize(value.size());
-
     // append the 4 byte value size
     // byte 6 - 9
-    for (const auto &b : value_size)
-    {
-        response.push_back(b);
-    }
+    response.push_back(value.size());
+    response.push_back(value.size() >> 8);
+    response.push_back(value.size() >> 16);
+    response.push_back(value.size() >> 24);
 
     // byte 10 - N
     // append the value
-    for (const auto &ele : value)
+    for (const auto& ele : value)
     {
         response.push_back(ele);
     }
